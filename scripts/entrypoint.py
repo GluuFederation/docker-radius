03662b432d3b69f35a6b2646b41ff0dc28be0110
iromli <isman.firmansyah@gmail.com>
2020-08-21 00:05:42 +0700

    pull certificate from secrets; fix #4

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -10,6 +10,7 @@ from pygluu.containerlib.persistence import sync_ldap_truststore
 from pygluu.containerlib.persistence import sync_couchbase_truststore
 from pygluu.containerlib.utils import get_server_certificate
 from pygluu.containerlib.utils import cert_to_truststore
+from pygluu.containerlib.utils import as_boolean
 
 
 def render_radius_properties(manager, src, dest):
@@ -52,7 +53,12 @@ def main():
     if persistence_type == "hybrid":
         render_hybrid_properties("/etc/gluu/conf/gluu-hybrid.properties")
 
-    get_server_certificate(manager.config.get("hostname"), 443, "/etc/certs/gluu_https.crt")
+    if not os.path.isfile("/etc/certs/gluu_https.crt"):
+        if as_boolean(os.environ.get("GLUU_SSL_CERT_FROM_SECRETS", False)):
+            manager.secret.to_file("ssl_cert", "/etc/certs/gluu_https.crt")
+        else:
+            get_server_certificate(manager.config.get("hostname"), 443, "/etc/certs/gluu_https.crt")
+
     cert_to_truststore(
         "gluu_https",
         "/etc/certs/gluu_https.crt",
